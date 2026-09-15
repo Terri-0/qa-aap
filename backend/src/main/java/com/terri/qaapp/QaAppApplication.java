fbc41b5c3778cc5e7fbec7119367a32589d3e306
Terri cero <rafcar2506@gmail.com>
2026-09-15 12:09:20 -0400

    Initial commit: backend and frontend skeletons

---
+++ backend/src/main/java/com/terri/qaapp/QaAppApplication.java
@@ -0,0 +1,13 @@
+package com.terri.qaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaAppApplication.class, args);
+	}
+
+}
